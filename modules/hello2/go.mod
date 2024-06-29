b46402885d3cfb33d316b8e08074e24b2977c767
Ivan Anisimov <ivananisimov2010@gmail.com>
2024-06-29 21:35:27 +0300

    use greetings package in hello2 module by external import

---
+++ modules/hello2/go.mod
@@ -0,0 +1,5 @@
+module github.com/valeter/go_tutorial/modules/hello2
+
+go 1.22.4
+
+require github.com/valeter/go_tutorial/modules/greetings v0.1.0 // indirect
---
+++ modules/hello2/hello2.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/valeter/go_tutorial/modules/greetings"
+)
+
+func main() {
+	fmt.Println(greetings.Hello("valter532"))
+}
